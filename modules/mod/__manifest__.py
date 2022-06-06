18cd7c7050d78f79fa9fe659272e3b6fc98a25be
sbm2020 <starlymulebo2020@gmail.com>
2022-06-06 12:45:01 +0100

    first commit

---
+++ modules/mod/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Module title",  # Module title
+    'summary': "Sommaire",  # Module subtitle phrase
+    'description': """  """,  # Supports reStructuredText(RST) format
+    'author': "Starly",
+    'website': "http://www.example.com",
+    'category': 'Tools',
+    'version': '14.0.1',
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
+
